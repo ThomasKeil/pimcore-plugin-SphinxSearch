8a3d41ecf1446a83c92092a252bdfa76b866c003
ThomasKeil <thomas@weblizards.de>
2013-07-24 08:42:37 +0200

    implemented a workaround for https://github.com/ThomasKeil/pimcore-plugin-SphinxSearch/issues/10

--- controllers/ClassController.php
+++ controllers/ClassController.php
@@ -111,7 +111,7 @@ class SphinxSearch_ClassController extends Pimcore_Controller_Action_Admin {
               case "input":
               case "textarea":
               case "wysiwyg":
-                $node->addAttribute("field_type", "sql_attr_string");
+                $node->addAttribute("field_type", "sql_field_string");
                 break;
               case "date":
               case "datetime":
